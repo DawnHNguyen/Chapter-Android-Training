ee0a0e459285ec1c41c3e936f1a428fd4593f33f
cherry <84829229+ngoc2k1@users.noreply.github.com>
2022-07-26 21:30:40 +0700

    cat 37 onClickListener

--- app/src/main/java/com/example/chapterandroidtraining/MainActivity2.kt
+++ app/src/main/java/com/example/chapterandroidtraining/MainActivity2.kt
@@ -2,10 +2,26 @@ package com.example.chapterandroidtraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.chapterandroidtraining.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding: ActivityMain2Binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main2
+        )
+        binding.buttonMain2Confirm.setOnClickListener {
+            binding.textviewMain2Title.text = getString(R.string.main_danhapkhothanhcong)
+        }
+
+        binding.buttonMain2Back.setOnClickListener {
+            binding.textviewMain2Title.text = getString(R.string.main_thongtinsanphamnhapkho)
+            binding.edittextTensanpham.text.clear()
+            binding.edittextNhasanxuat.text.clear()
+            binding.edittextMain2Soluong.text.clear()
+            binding.edittextMain2Ngaynhapkho.text.clear()
+            binding.editextMain2Loaisanpham.text.clear()
+        }
     }
 }
