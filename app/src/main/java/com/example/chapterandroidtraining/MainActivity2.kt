396c0eaecab254ff9679844b20ce22039d38ea5f
cherry <84829229+ngoc2k1@users.noreply.github.com>
2022-08-22 17:56:35 +0700

    navigate and transfer data between 2 fragment

--- app/src/main/java/com/example/chapterandroidtraining/MainActivity2.kt
+++ app/src/main/java/com/example/chapterandroidtraining/MainActivity2.kt
@@ -1,27 +1,14 @@
 package com.example.chapterandroidtraining
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.example.chapterandroidtraining.databinding.ActivityMain2Binding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chapterandroidtraining.utlis.showToast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMain2Binding = DataBindingUtil.setContentView(
-            this, R.layout.activity_main2
-        )
-        binding.buttonMain2Confirm.setOnClickListener {
-            binding.textviewMain2Title.text = getString(R.string.main_danhapkhothanhcong)
-        }
-
-        binding.buttonMain2Back.setOnClickListener {
-            binding.textviewMain2Title.text = getString(R.string.main_thongtinsanphamnhapkho)
-            binding.edittextMain2Tensanpham.text.clear()
-            binding.edittextMain2Nhasanxuat.text.clear()
-            binding.edittextMain2Soluong.text.clear()
-            binding.edittextMain2Ngaynhapkho.text.clear()
-            binding.editextMain2Loaisanpham.text.clear()
-        }
+        setContentView(R.layout.activity_main_2)
     }
+
 }
